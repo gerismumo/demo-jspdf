c74d5f2561c88994f3c7755b03f2abe3a0292ea5
Gerald <103599982+gerismumo@users.noreply.github.com>
2024-04-27 00:50:34 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from 'react';
+import { jsPDF } from "jspdf";
+import Report from './Report';
+// import "jspdf-autotable";
+
+
+const Home = () => {
+    const letters = [
+        {
+          recipientName: "Recipient 1",
+          recipientAddress: "Address 1",
+          reason: "Reason 1"
+        },
+        {
+          recipientName: "Recipient 2",
+          recipientAddress: "Address 2",
+          reason: "Reason 2"
+        },
+      ];
+  
+
+  const handleDownloadPdf = () => {
+    const generatePDF = (index) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const doc = new jsPDF('l', 'mm', [1500, 1400]);
+          const reportContainer = document.getElementById(`pdf-content-${index}`);
+          //center the letter
+
+
+          doc.html(reportContainer, {
+            callback: function (pdf) {
+              pdf.save(`letter_${index + 1}.pdf`);
+              resolve();
+            },
+            x: 12,
+            y: 12,
+          });
+        }, 1000); 
+      });
+    };
+  
+    const generatePDFsSequentially = async () => {
+      for (let index = 0; index < letters.length; index++) {
+        await generatePDF(index);
+      }
+    };
+  
+    generatePDFsSequentially();
+  };
+  
+
+  return (
+    <div >
+       {letters.map((letter, index) => (
+        <div key={index} style={{ marginBottom: '20px' }}>
+          <Report
+            recipientName={letter.recipientName}
+            recipientAddress={letter.recipientAddress}
+            reason={letter.reason}
+            index={index}
+          />
+        </div>
+        ))}
+        <div className="btn">
+            <button onClick={handleDownloadPdf}>Download PDF</button>
+        </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Report.jsx
@@ -0,0 +1,41 @@
+// Report.js
+
+import React from 'react';
+
+const Report = ({ recipientName, recipientAddress, reason, index }) => {
+  return (
+    <div id={`pdf-content-${index}`} style={{ width: "210mm" }} className="formal-letter">
+      <div className="header">
+        <div className="logo">
+          <img src="../../images/download (1).png" alt=""
+            style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+        </div>
+        <h2>John Doe</h2>
+        <p>123 Main Street</p>
+        <p>City, State, Zip Code</p>
+        <p>Email: johndoe@example.com</p>
+        <p>Phone: (123) 456-7890</p>
+        <p>Date: April 26, 2024</p>
+      </div>
+      <div className="content">
+        <p>{`Recipient's Name: ${recipientName}`}</p>
+        <p>{`Recipient's Address: ${recipientAddress}`}</p>
+        <p>City, State, Zip Code</p>
+        <br />
+        <p>Dear {recipientName},</p>
+        <br />
+        <p>
+          I am writing to express my interest in {reason}.
+        </p>
+        <p>
+          [Body of the letter, expressing thoughts, intentions, requests, etc.]
+        </p>
+        <br />
+        <p>Sincerely,</p>
+        <p>John Doe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Report;
